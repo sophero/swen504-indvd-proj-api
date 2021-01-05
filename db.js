eadc7ea79f8595be0bd41cecd8b8f839bba9ce30
Sophia Di <backwardsxmarathon@gmail.com>
2021-01-06 11:57:18 +1300

    Connect to db

---
+++ db.js
@@ -0,0 +1,28 @@
+const MongoClient = require("mongodb").MongoClient;
+
+// mongodb instance details
+const dbUser = "db-user";
+const dbPass = process.env.MONGODB_ATLAS_DB_PASS;
+const dbName = "gratitudedb";
+// const dbConnectionUrl = "mongodb+srv://" + dbUser + ":" + dbPass + "@cluster0.5pwke.mongodb.net/gratitudedb?retryWrites=true&w=majority";
+const dbConnectionUrl = `mongodb+srv://${dbUser}:${dbPass}@cluster0.5pwke.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+function initialise(dbName, dbCollectionName, successCallback, failureCallback) {
+    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err);
+
+        } else {
+
+            console.log("[MongoDB connection] SUCCESS");
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            successCallback(dbCollection);
+        }
+    });
+}
+
+module.exports = {
+    initialise
+};
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const server = express();
-
 const body_parser = require("body-parser");
-// parse JSON
+
 server.use(body_parser.json());
 
 const port = process.env.PORT || 4000;
+
+// connect to db
+const db = require("./db");
+const dbName = "gratitudedb";
+const collectionName = "gratitude_posts";
+db.initialise(
+    dbName,
+    collectionName,
+    // success cb
+    function(dbCollection) {
+        dbCollection.find().toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    // failure cb
+    function(err) {
+        throw err;
+    }
+);
 
 server.listen(port, () => {
    console.log(`Server listening at ${port}`);
